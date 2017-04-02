2c6d4d9e6efc849c14b76904549d3f5ae0b07318
Henry Leon <helg18@gmail.com>
2017-04-01 20:51:59 -0400

    Doing responsive form to send a new msg - customize my messages |Alignment - Color|

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ $('#messages').height('300px');
 
 //establishing the conection
 var socket = io.connect('http://52.18.22.122:8000', {'forceNew':true});
-
+						//change this url to YOUR SERVER  URL
 
 //listener for messages
 socket.on('messages', function(data){
@@ -12,10 +12,22 @@ socket.on('messages', function(data){
 
 //runner for display data on chat box
 function writer(data){
+
+	//Var to customize messages
+	var my_nickname = $('#name_user').val();
+	var my_messages = '';
+	var my_color    = 'blue-grey darken-1';
+
+
 	var html = data.map(function(message, index){
+		if (message.nickname == my_nickname) {
+			my_messages = 'offset-s4 offset-m4 offset-l4';
+			my_color    = 'green darken-4';
+		}
+
 		return (`
-			<div class="col s12 m8">
-				<div class="card blue-grey darken-1">
+			<div class="col s8 m8 l8 ${my_messages}"">
+				<div class="card ${my_color}">
 					<div class="card-content white-text">
 						<span class="card-title"> ${message.nickname}</span>
 						<p>${message.text}</p>
